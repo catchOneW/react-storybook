021c38249714d443b4b2fce471ce3e5b6281c092
kaiqiw <1132620342@qq.com>
2019-07-05 15:32:19 +0800

    1

--- packages/menu/index.js
+++
@@ -1,9 +0,0 @@
-import Menu from './Menu';
-import SubMenu from './SubMenu';
-import MenuItem from './MenuItem';
-import './index.scss'
-
-Menu.SubMenu = SubMenu;
-Menu.Item = MenuItem;
-
-export default Menu
